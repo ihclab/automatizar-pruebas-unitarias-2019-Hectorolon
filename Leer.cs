96bc7a1e41a0a95e1973108623278dffffc0ec2f
Hectorolon <hrolon@ucol.mx>
2019-03-20 23:24:59 -0600

    Problema 002 modificado

--- Leer.cs
+++ Leer.cs
@@ -1,31 +1,39 @@
-using System;using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 using System.Collections;
 
-namespace TextFileReader_csharp
+namespace LeerArchivo
 {
+    class Leer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr =
+                    new StreamReader("C:/Users/hrolo/Desktop/automatizar-pruebas-unitarias-2019-Hectorolon/Problema 01.txt"))
+                {
+                    string linea;
 
-class Class1
-{
-static void Main(string[] args)
-{
-StreamReader sr = new StreamReader("C:\Users\hrolo\Desktop\automatizar-pruebas-unitarias-2019-Hectorolon\Problema 01.txt");
-string linea="";
-ArrayList texto = new ArrayList();
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linea);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("El archivo no se puede leer");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
 
-while (linea != null)
-{
-linea = sr.ReadLine();
-if (linea != null)
-texto.Add(linea);
+        }
+    }
 }
-srr.Close();
-
-foreach (string sOutput in texto)
-Console.WriteLine(sOutput);
-Console.ReadLine();
-}
-}
-}
-
 
 
