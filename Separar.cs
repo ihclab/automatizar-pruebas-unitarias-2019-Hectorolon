734a03208f7822c257769824426c30d39a4e4534
Hectorolon <hrolon@ucol.mx>
2019-03-20 23:55:21 -0600

    Problema 003

---
+++ Separar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Separar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr =
+                    new StreamReader("C:/Users/hrolo/Desktop/automatizar-pruebas-unitarias-2019-Hectorolon/Problema 01.txt"))
+            {
+                string linea;
+
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    string[] separados = linea.Split(':');
+                    foreach (string separado in separados)
+                    {
+                        Console.WriteLine(separado);
+                    }
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
