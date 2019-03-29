a2bd17ff216a153d9bd272d2e3e80633d8f6d8da
Hectorolon <hrolon@ucol.mx>
2019-03-29 08:53:53 -0600

    Problema 004

---
+++ AutomatizadorPruebasunitarias/AutomatizadorPruebasunitarias/Medias.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizadorPruebasunitarias
+{
+    class Medias
+    {
+        public static double mediaAritmetica(params int[] vals)
+        {
+            int valores = 0;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                valores += vals[i];
+            }
+            return valores / vals.Length;
+        }
+        
+        private static double raizEnesima(double x, int n)
+        {
+            return Math.Pow(x, 1 / n);
+        }
+        
+        public double mediaGeometrica(params int[] vals)
+        {
+            int valores = 1;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                valores *= vals[i];
+            }
+            return raizEnesima(valores, vals.Length);
+        }
+        
+        public static double mediaArmonica(params int[] vals)
+        {
+            double valores = 0;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                valores += 1 / vals[i];
+            }
+            return vals.Length / valores;
+        }
+    }
+}
---
+++ AutomatizadorPruebasunitarias/AutomatizadorPruebasunitarias/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizadorPruebasunitarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Medias medias = new Medias();
+            
+            try
+            {                
+                using (StreamReader sr =
+                    new StreamReader("../../Problema 01.txt"))
+                {
+                    string linea;
+                    
+
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        string[] separados = linea.Split(':');
+
+                        foreach (string separado in separados)
+                        {
+                            string[] var = separados[2].Split(' ');
+                            Console.WriteLine(separado);
+                            int[] parametros = new int[var.Length];
+
+                            try
+                            {
+                                for (int i = 0; i < parametros.Length; i++)
+                                {
+                                    parametros[i] = int.Parse(var[i]);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                if (separados[2] == "NULL")
+                                    parametros = null;
+                                //Console.WriteLine(e.Message);
+                            }
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("El archivo no se puede leer");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();            
+        }
+    }
+}
